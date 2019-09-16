1268624b897518bac0d3929965411809d9462c7c
Nicolas-62 <droffig@hotmail.com>
2019-09-16 23:04:43 +0200

    modif nom temperature.java et ajout sortis à copylist

--- CodeInGame/src/com/testsEntretien/java/testsCopyLists.java
+++ CodeInGame/src/com/testsEntretien/java/testsCopyLists.java
@@ -32,21 +32,25 @@ public class testsCopyLists {
 		List<Integer> dest = Arrays.asList(4,5,6);
 		Collections.copy(dest, source);
 		System.out.println("copy 1 : "+dest);
+		// copy 1 : [1, 2, 3]
 		
 		List<Integer> source2 = Arrays.asList(1, 2, 3);
 		List<Integer> dest2 = Arrays.asList(5, 6, 7, 8, 9, 10);
 		Collections.copy(dest2, source2);
 		System.out.println("copy 2 : "+dest2);
+		// copy 2 : [1, 2, 3, 8, 9, 10]
 		
 		List<String> copy = list.stream()
 				  .skip(1)
 				  .collect(Collectors.toList());
 		System.out.println("copy stream, skip"+copy);
+		// copy stream, skip[Lourdel]
 		
 		List<String> copy2 = list.stream()
 				  .filter(s -> s.length() > 10)
 				  .collect(Collectors.toList());
 		System.out.println("copy2 stream, skip"+copy2);
+		// copy2 stream, skip[]
 		
 		Flower flo = new Flower(10);
 		List<Flower> listFlowers = new ArrayList<Flower>();
@@ -57,6 +61,7 @@ public class testsCopyLists {
 				  .filter(f -> f.getPetals() > 3)
 				  .collect(Collectors.toList());
 		System.out.println("flowers : "+flowers);
+		// flowers : [je suis une fleur et j'ai 4 pétales, je suis une fleur et j'ai 10 pétales]
 	}
 
 }
