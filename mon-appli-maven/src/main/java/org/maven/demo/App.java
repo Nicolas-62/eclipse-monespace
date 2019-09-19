9271b192892dd0945fec2515d58da90cbe8087d3
Nicolas-62 <droffig@hotmail.com>
2019-09-19 10:54:10 +0200

    Maven tuto first commit

---
+++ mon-appli-maven/src/main/java/org/maven/demo/App.java
@@ -0,0 +1,15 @@
+package org.maven.demo;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//        commentaire
+    }
+}
